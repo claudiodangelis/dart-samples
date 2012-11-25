7311475c5cc4ba428f525c20ade1e4c2bd9f1d33
Claudio d'Angelis <claudiodangelis@gmail.com>
2012-11-25 15:41:54 +0100

    sys_profiler works on linux only

--- scripts/sys_profiler.dart
+++ scripts/sys_profiler.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 // Prints informations about current systems
-// UNIX only
+// Linux only
 
 String kilobytes2human(num value){
 
@@ -19,42 +19,50 @@ String kilobytes2human(num value){
 
 main(){
 
-  Process.run('uname', ['-a']).then((ProcessResult results){
-    print(results.stdout.split('\n')[0]);
-  });
+  Process.run('uname',[]).then((ProcessResult results){
+    if(results.stdout=='Linux'){
 
-  Process.run('uptime',[]).then((ProcessResult results){
-    print('Uptime: ${results.stdout}');
-  });
+      Process.run('uname', ['-a']).then((ProcessResult results){
+        print(results.stdout.split('\n')[0]);
+      });
 
-  Process.run('cat', ['/proc/meminfo']).then((ProcessResult results){
-      // /proc/meminfo returns values expressed in kilobytes
-      print('RAM: ${kilobytes2human(
-          int.parse(
-              results.stdout.split('\n')[0].replaceAll(new RegExp('[^0-9]'), ''))
-            )}'
-          );
-       });
+      Process.run('uptime',[]).then((ProcessResult results){
+        print('Uptime: ${results.stdout}');
+      });
 
-  Process.run('df',[]).then((ProcessResult results){
-    for ( var line in results.stdout.split('\n')){
-      if( line.contains('/dev/disk/')){
-        print('Disk space: ${kilobytes2human(int.parse(line.split(' ')[2]))}');
-      }
+      Process.run('cat', ['/proc/meminfo']).then((ProcessResult results){
+        // /proc/meminfo returns values expressed in kilobytes
+        print('RAM: ${kilobytes2human(
+            int.parse(
+                results.stdout.split('\n')[0].replaceAll(new RegExp('[^0-9]'), ''))
+        )}'
+        );
+      });
+
+      Process.run('df',[]).then((ProcessResult results){
+        for ( var line in results.stdout.split('\n')){
+          if( line.contains('/dev/disk/')){
+            print('Disk space: ${kilobytes2human(int.parse(line.split(' ')[2]))}');
+          }
+        }
+      });
+
+      Process.run('cat', ['/proc/cpuinfo']).then((ProcessResult results){
+        for ( var line in results.stdout.split('\n')){
+
+          if (line.contains("processor")){
+            print(line.replaceAll("\t", ''));
+          }
+
+          if(line.contains("model name")){
+            print(line.replaceAll(new RegExp('model name.*: '), ''));
+          }
+        }
+      });
+    }else{
+      print('Sorry, this script doesn\'t work on non-Linux operating systems.');
     }
   });
 
-  Process.run('cat', ['/proc/cpuinfo']).then((ProcessResult results){
-    for ( var line in results.stdout.split('\n')){
-
-      if (line.contains("processor")){
-        print(line.replaceAll("\t", ''));
-      }
-
-      if(line.contains("model name")){
-        print(line.replaceAll(new RegExp('model name.*: '), ''));
-      }
-    }
-  });
 
 }
